e7ab97957ff9adf8729157bda54e1bc7ad9f9c06
William Cooper <wcooperhockey@gmail.com>
2019-06-30 01:05:19 -0400

    some file transfers

--- application.py
+++ application.py
@@ -1,3 +1,10 @@
-from . import Particle, StarSystem
+# from . import Particle, StarSystem
 from flask import Flask, flash, jsonify, redirect, session, render_template, request, session
 from flask_session import Session
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")

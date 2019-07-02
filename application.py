5e1780990e5c90f3843e127393fb0ed1882ec86a
William Cooper <wcooperhockey@gmail.com>
2019-07-02 10:33:11 -0400

    future page

--- application.py
+++ application.py
@@ -8,3 +8,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/error")
+def error():
+    return render_template("error.html")
+
+@app.route("/Future")
+def future():
+    return render_template("future.html")

2e8aa8b87117f89dc7c0427ab5e9b50440d28b8c
William Cooper <wcooperhockey@gmail.com>
2019-07-02 16:02:11 -0400

    minimal

--- application.py
+++ application.py
@@ -24,10 +24,24 @@ def future():
 @app.route("/AstroSim", methods=["GET", "POST"])
 def AstroSim():
     if request.method == "POST":
-        return redirect("/Sim")
+        G = 39.476926421373
+        stars = [Particle(m=1,x=9),Particle(m=1,x=-9)] #Particle(m=0.1221,x=8700)
+        stars[0].vy = np.sqrt(G*stars[1].m/(2*(stars[0].a+stars[1].a)))
+        stars[1].vy = -np.sqrt(G*stars[0].m/(2*(stars[0].a+stars[1].a)))
+        ast = Particle(m=0,x=10)
+        sys = StarSystem(stars,ast,n_ast=10,seed=6)
+        t_start = 0
+        t_stop = 100
+        iterations = 100
+        sim = sys.initiateSim()
+        df_particles = sys.integrateSim(t_start,t_stop,iterations,sim,dt="linear")
+        sys.writeData(df_particles)
+        df = sys.finalPositionsVelocities(df_particles,printData=True,saveData=True)
+        sys.plotAll(df_particles,dim=3,lim=25)
+        # maybe move this function into the rebound file. Figure out how to display the pdf without having to
+        # save it somewhere, as well as links to the downloadable csv files for particles. 
+        info = []
+        info.append("a")
+        return render_template("Sim.html", info=info)
     else:
         return render_template("AstroSim.html")
-
-@app.route("/Sim")
-def Sim():
-    return render_template("Sim.html")
